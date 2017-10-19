62eddc752b6760eaad11c92ed00a37d8ee57ca31
Klackky <natalia.volchatova@gmail.com>
2017-10-19 14:55:27 -0400

    пробелы

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-var minify = require ("gulp-csso");
-var rename = require ("gulp-rename");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
 var imagemin = require('gulp-imagemin');
-var webp = require ("gulp-webp");
-var run = require ("run-sequence");
-var del = require ("del");
+var webp = require("gulp-webp");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -21,47 +21,53 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("build/css"))
-	.pipe (minify())
-	.pipe (rename("style.min.css"))
-	.pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task ("webp", function() {
-	   return gulp.src ("img/**/*.{png, jpg}")
-	  .pipe (webp({quality:90}))
-	  .pipe (gulp.dest ("build/img"));
+gulp.task("webp", function() {
+  return gulp.src("img/**/*.{png, jpg}")
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest("build/img"));
 });
-  
+
 gulp.task("images", function() {
-	return gulp.src("img/**/*.{png,jpg,svg}")
-	.pipe(imagemin([
-	  imagemin.optipng({optimizationLevel: 3}),
-	  imagemin.jpegtran({progressive: true}),
-	  imagemin.svgo()
-	]))
-	.pipe(gulp.dest("build/img"));
+  return gulp.src("img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.jpegtran({
+        progressive: true
+      }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("clean", function(){
-	return del ("build");
+gulp.task("clean", function() {
+  return del("build");
 });
 
-  gulp.task("copy", function(){
+gulp.task("copy", function() {
   return gulp.src([
-  "fonts/**/*.{woff,woff2}",
-  "img/**",
-  "js/**"
-  ], {
-	  base:"."
-  })
-.pipe(gulp.dest("build"));
- }); 
- 
-  gulp.task("build", function (done){
-	  run("clean", "copy", "style", "images", "webp", done);
- });
-  
+      "fonts/**/*.{woff,woff2}",
+      "img/**",
+      "js/**"
+    ], {
+      base: "."
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function(done) {
+  run("clean", "copy", "style", "images", "webp", done);
+});
+
 gulp.task("serve", function() {
   server.init({
     server: "build/",
@@ -70,7 +76,7 @@ gulp.task("serve", function() {
     cors: true,
     ui: false
   });
-  
+
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
 });
--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
-
-var toggle = document.querySelector (".main-nav__button");
-var dropdown = document.querySelector (".main-nav__dropdown");
-var menu = document. querySelector (".main-nav__user-menu");
+var toggle = document.querySelector(".main-nav__button");
+var dropdown = document.querySelector(".main-nav__dropdown");
+var menu = document.querySelector(".main-nav__user-menu");
 
 dropdown.classList.add("main-nav__hidden");
 menu.classList.add("main-nav__hidden");
 
 toggle.addEventListener("click", function() {
-	if (dropdown.classList.contains("main-nav__hidden")) {
-		dropdown.classList.remove("main-nav__hidden");
-		dropdown.classList.add("main-nav__show");
-	} else {
-		dropdown.classList.add("main-nav__hidden");
-		dropdown.classList.remove("main-nav__show");
-	}
+  if (dropdown.classList.contains("main-nav__hidden")) {
+    dropdown.classList.remove("main-nav__hidden");
+    dropdown.classList.add("main-nav__show");
+  } else {
+    dropdown.classList.add("main-nav__hidden");
+    dropdown.classList.remove("main-nav__show");
+  }
   if (menu.classList.contains("main-nav__hidden")) {
     menu.classList.remove("main-nav__hidden");
     menu.classList.add("main-nav__show");
@@ -30,34 +29,34 @@ toggle.addEventListener("click", function() {
   }
 });
 
-
-
 function initMap() {
-        var uluru = {lat: 59.938794, lng: 30.323083};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 17,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map,
-          icon: 'img/icon-map-pin.svg',
-        });
+  var uluru = {
+    lat: 59.938794,
+    lng: 30.323083
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    icon: 'img/icon-map-pin.svg',
+  });
+}
 
+var link = document.querySelector(".button--order-promo");
+var popup = document.querySelector(".popup__wrapper");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("popup__wrapper--show");
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (popup.classList.contains("popup__wrapper--show")) {
+        popup.classList.remove("popup__wrapper--show");
       }
-
-      var link = document.querySelector (".button--order-promo");
-      var popup = document.querySelector (".popup__wrapper");
-
-      link.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.add("popup__wrapper--show");
-
-      window.addEventListener("keydown", function(event) {
-       if (event.keyCode === 27) {
-         if (popup.classList.contains("popup__wrapper--show")) {
-           popup.classList.remove("popup__wrapper--show");
-         }
-       }
-      });
-      });
+    }
+  });
+});
